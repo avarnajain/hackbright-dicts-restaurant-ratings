c3ec33daa1faacc276d0daa5697272d83d2aab7e
vagrant <vagrant@vagrant.vm>
2019-02-01 00:01:16 +0000

    User input added, not sure how to keep user input saved in the file

--- ratings.py
+++ ratings.py
@@ -1,52 +1,66 @@
 """Restaurant rating lister."""
 
-
 # put your code here
-restaurant_dict = {}
 
 def restaurant_ratings(filename):
-    """ Convert restaurant file into dictionary"""
+    """ Convert restaurant file into list with name and ratings"""
     #open file
     file_object = open(filename)
-
-    #creat empty data structures
-    combined_list = []
     
+    #create empty list
+    restaurant_list = []
+
     #read and clean file, add to list
     for line in file_object:
         line = line.rstrip().split(':')
-        combined_list += [line]
+        restaurant_list += [line]
+
+    return restaurant_list
+    
+
+def sort_restaurants(restaurant_list):
+    """ Sort list of restaurants, convert to dictionary and print ratings"""
     
     #sort list
-    combined_list.sort()
+    restaurant_list.sort()
+
+    #create empty dictionary
+    restaurant_dict = {}
 
     #convert sorted list to dictionary
-    for restaurant in combined_list:
+    for restaurant in restaurant_list:
         restaurant_dict[restaurant[0]] = restaurant[1]
 
-    print_ratings(restaurant_dict)
     #print restaurants and their rating
-    
-
-def print_ratings(restaurant_dictionary):
-    """print restaurant ratings"""
     for restaurant, rating in restaurant_dict.items():
         print(restaurant, 'is rated at', rating)
+    
 
-#call function
-restaurant_ratings('scores.txt')
-
-print('*' * 80)
-
-#Get user input
-user_restaurant = input('Add the name of the restaurant you wish to rate: ')
-user_rating = input('Rate {} out of 5 stars: '.format(user_restaurant))
-
-#Add use input to global dictionary
-restaurant_dict[user_restaurant] = user_rating
-
+#call function to open and parse file, and then sort it
+sort_restaurants(restaurant_ratings('scores.txt'))
 
 print('*' * 80)
 
-#call printing function
-print_ratings(restaurant_dict)
+def add_restaurant_rating():
+    """Get user to add and rate restauant and print entire ratings list"""
+    
+    #Get user input
+    user_restaurant = input('Add the name of the restaurant you wish to rate: ')
+    user_rating = input('Rate {} out of 5 stars: '.format(user_restaurant))
+
+    #call function to return list of restaurants from earlier
+    restaurant_list = restaurant_ratings('scores.txt')
+
+    #add user input to list
+    restaurant_list.append([user_restaurant.capitalize(), user_rating])
+    
+    print('*' * 80)
+    
+    #call sorting function on list
+    sort_restaurants(restaurant_list)
+
+
+print('*' * 80)
+
+#ask for user input
+add_restaurant_rating()
